4a4e13499719a2471e82b4011a92222005b2bc5d
Nagarjun45 <nagarjunhonrao.ai4577@gmail.com>
2025-06-08 22:19:36 +0530

    Added Redux Files

--- my-redux-app/src/Components/Redux/Reducers/reducers.jsx
+++ my-redux-app/src/Components/Redux/Reducers/reducers.jsx
@@ -7,10 +7,10 @@ const initialState = {
 export const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case INCREMENT:
-            return { ...state, count: state.count + 1 }
+            return { count: state.count + 1 }
 
         case DECREMENT:
-            return { ...state, count: state.count - 1 }
+            return { count: state.count - 1 }
 
         case RESET:
             return { count: 0 }
